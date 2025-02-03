5793b53fcb17ddc7f11f329f63255bfaf78bd3bf
Rishi Pal <akshat@supereps.ai>
2025-02-03 19:04:41 +0530

    fix build error

--- vite.config.ts
+++ vite.config.ts
@@ -1,24 +1,16 @@
-import path from "path"
-import react from "@vitejs/plugin-react"
-import { defineConfig } from "vite"
+// vite.config.js
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import path from 'path';
 
-const basenameProd = '/react-shadcn-starter'
-
-export default defineConfig(({ command }) => {
-  const isProd = command === 'build'
-
-  return {
-    base: isProd ? basenameProd : '',
-    plugins: [react()],
-    resolve: {
-      alias: {
-        "@": path.resolve(__dirname, "./src"),
-      },
+export default defineConfig({
+  plugins: [react()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
     },
-    define: {
-      global: {
-        basename: isProd ? basenameProd : '',
-      },
-    },
-  }
-})
+  },
+  build: {
+    target: 'esnext', // or 'es2015' depending on your needs
+  },
+});
